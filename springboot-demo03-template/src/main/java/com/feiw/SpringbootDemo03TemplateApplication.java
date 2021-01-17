69462435367f7f677f091fdf9985b8668d5ac8ae
pizisummer <piz-summer@163.com>
2021-01-17 15:48:21 +0800

    demo03

---
+++ springboot-demo03-template/src/main/java/com/feiw/SpringbootDemo03TemplateApplication.java
@@ -0,0 +1,13 @@
+package com.feiw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo03TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo03TemplateApplication.class, args);
+    }
+
+}
---
+++ springboot-demo03-template/src/test/java/com/feiw/SpringbootDemo03TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feiw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo03TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
